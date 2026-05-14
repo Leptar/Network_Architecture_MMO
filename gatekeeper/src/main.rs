eb4fc1bcee60a67e48d4879f2b14b43cc26564c4
Léo Cheikh-Boukal <leo.ultimate34@gmail.com>
2026-05-14 18:14:27 -0400

    update redis_pool
    base redis with docker, will have some change in next work session

--- gatekeeper/src/main.rs
+++ gatekeeper/src/main.rs
@@ -5,7 +5,10 @@ mod redis_pool;
 
 #[launch]
 fn rocket() -> _ {
+    let redis_pool = redis_pool::create_pool();
+
     rocket::build()
+        .manage(redis_pool)
         .mount("/", routes![
             handlers::health,
             handlers::login
