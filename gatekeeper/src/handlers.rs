ac2481bdf3c49d59dbb11b1993cf5f057739abcd
Léo Cheikh-Boukal <leo.ultimate34@gmail.com>
2026-05-14 17:16:28 -0400

    Update handlers.rs
    add basic logic for login. stub for now

--- gatekeeper/src/handlers.rs
+++ gatekeeper/src/handlers.rs
@@ -1,5 +1,6 @@
 ﻿use rocket::{get, post};
 use rocket::serde::json::Json;
+use rocket::http::Status;
 use serde::{Serialize, Deserialize};
 
 // HEALTH ENDPOINT PART
@@ -41,6 +42,24 @@ pub struct ErrorResponse {
 }
 
 #[post("/login", data = "<login_data>")]
-pub fn login(login_data: Json<LoginRequest<'_>>) -> Json<String> {
-    Json(format!("Salut {}, j'ai bien reçu ton mot de passe.", login_data.username))
+pub fn login(login_data: Json<LoginRequest<'_>>) -> Result<Json<LoginResponse>, (Status, Json<ErrorResponse>)> {
+    let user = login_data.username;
+    let pass = login_data.password;
+
+    // Stub : je verifie pas reelement quoi que se soit just le user doit pas etre vide et le pass
+    // c'est 1234.
+    if !user.is_empty() && pass == "1234" {
+        Ok(Json(LoginResponse {
+            player_id: "un-id-unique-temporaire".to_string(),
+            server: ServerInfo {
+                ip: "127.0.0.1".to_string(),
+                port: 7001,
+                zone: "zone_A".to_string(),
+            },
+        }))
+    } else {
+        Err((Status::Unauthorized, Json(ErrorResponse {
+            error: "Nom d'utilisateur ou mot de passe incorrect".to_string()
+        })))
+    }
 }
