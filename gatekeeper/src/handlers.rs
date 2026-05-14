ff24c07abfdc5f9c7cf14c71488a751fea419e4e
Léo Cheikh-Boukal <leo.ultimate34@gmail.com>
2026-05-14 17:07:54 -0400

    Update handlers.rs
    Add login endpoint

--- gatekeeper/src/handlers.rs
+++ gatekeeper/src/handlers.rs
@@ -1,7 +1,8 @@
-﻿use rocket::get;
+﻿use rocket::{get, post};
 use rocket::serde::json::Json;
-use serde::Serialize;
+use serde::{Serialize, Deserialize};
 
+// HEALTH ENDPOINT PART
 #[derive(Serialize)]
 pub struct HealthResponse {
     pub status: String,
@@ -12,4 +13,34 @@ pub fn health() -> Json<HealthResponse> {
     Json(HealthResponse {
         status: "ok".to_string(),
     })
+}
+
+//LOGIN ENDPOINT PART
+#[derive(Deserialize)]
+pub struct LoginRequest<'r> {
+    pub username: &'r str,
+    pub password: &'r str,
+}
+
+#[derive(Serialize)]
+pub struct ServerInfo {
+    pub ip: String,
+    pub port: u16,
+    pub zone: String,
+}
+
+#[derive(Serialize)]
+pub struct LoginResponse {
+    pub player_id: String,
+    pub server: ServerInfo,
+}
+
+#[derive(Serialize)]
+pub struct ErrorResponse {
+    pub error: String,
+}
+
+#[post("/login", data = "<login_data>")]
+pub fn login(login_data: Json<LoginRequest<'_>>) -> Json<String> {
+    Json(format!("Salut {}, j'ai bien reçu ton mot de passe.", login_data.username))
 }
