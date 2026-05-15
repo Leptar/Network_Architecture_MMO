5c97185888d5f94650eaf40a50bcc518475cca39
Oridoshi <tom18dunet@gmail.com>
2026-05-15 06:03:51 +0200

    Desc : - Ajout de la méthode heartbeat_listener - passage du main en async - lancement de HOT_SERVERS_MIN server au démarage - docker ce sup automatiquement via --rm pour evité les serv doublon
    Points à implémenter :
    - [X] Tâche async `heartbeat_listener` (tokio UDP socket)
    - [ ] Mise à jour Redis avec `redis-rs` (commandes `HSET` + `EXPIRE`)
    - [ ] Tâche async `scaler_loop` avec `tokio::time::interval`
    - [ ] `count_available_servers` : `SCAN` + `HGET status`
    - [ ] `spawn_server`

--- orchestrator/src/main.rs
+++ orchestrator/src/main.rs
@@ -1,5 +1,7 @@
 use std::process::Command;
 use std::sync::atomic::{AtomicUsize, Ordering};
+use tokio::net::UdpSocket;
+use shared::Heartbeat;
 
 //Variable to manage game server
 const HOT_SERVERS_MIN: usize = 2;
@@ -9,23 +11,26 @@ const UPDATE_REDIS_TIME: usize = 5;
 
 static DGS_ID: AtomicUsize = AtomicUsize::new(0);
 
-fn start_servers()
+async fn start_servers()
 {
-    let id = DGS_ID.fetch_add(1, Ordering::Relaxed);
-    let nom_conteneur = format!("dgs-{}", id);
-    let orch_addr = format!("ORCH_ADDR=host.docker.internal:{}", ORCH_PORT);
+    for _ in 0..HOT_SERVERS_MIN {
+        let id = DGS_ID.fetch_add(1, Ordering::Relaxed);
+        let nom_conteneur = format!("dgs-{}", id);
+        let orch_addr = format!("ORCH_ADDR=host.docker.internal:{}", ORCH_PORT);
 
-    Command::new("docker")
-        .args(&[
-            "run",
-            "--name",
-            &nom_conteneur,
-            "-e",
-            &orch_addr,
-            "dgs-image",
-        ])
-        .spawn()
-        .expect("Failed to start game server 1");
+        Command::new("docker")
+            .args(&[
+                "run",
+                "--name",
+                &nom_conteneur,
+                "-e",
+                &orch_addr,
+                "--rm",
+                "dgs-image",
+            ])
+            .spawn()
+            .expect("Failed to start game server 1");
+    }
 }
 
 fn stop_servers()
@@ -33,6 +38,31 @@ fn stop_servers()
 
 }
 
-fn main() {
-    start_servers();
+
+async fn heartbeat_listener (){
+    //format : HEARTBEAT { id, ip, port, zone, player_count }
+    let socket = UdpSocket::bind(("0.0.0.0", ORCH_PORT as u16)).await.expect("Failed to start heartbeat listener");
+
+    loop {
+        let mut buf = [0; 1024];
+        let (len, addr) = socket.recv_from(&mut buf).await.expect("Failed to receive heartbeat");
+        let msg = String::from_utf8_lossy(&buf[..len]);
+        println!("Received heartbeat from {}: {}", addr, msg);
+
+        if let Ok(heartbeat) = serde_json::from_str::<Heartbeat>(&msg) {
+            println!("Parsed heartbeat: {:?}", heartbeat);
+        } else {
+            println!("Failed to parse heartbeat, ignoring...");
+        }
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    tokio::spawn(async move {start_servers().await;});
+
+    tokio::spawn(async move {heartbeat_listener().await;});
+
+    tokio::signal::ctrl_c().await.expect("Failed to listen for Ctrl+C");
+    println!("Shutting down orchestrator...");
 }
