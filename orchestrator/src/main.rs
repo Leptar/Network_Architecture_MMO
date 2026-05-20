498078817bd12e920d79e678ea321b8d9efa1e60
Oridoshi <tom18dunet@gmail.com>
2026-05-20 23:50:52 +0200

    Desc : - Ajout de la méthode pour savoir le nb de serv available - Ajout du scaler loop qui vérifie si il y a le bon nb de serv et en lance au besoin
    Points à implémenter :
    - [X] Tâche async `heartbeat_listener` (tokio UDP socket)
    - [X] Mise à jour Redis avec `redis-rs` (commandes `HSET` + `EXPIRE`)
    - [X] Tâche async `scaler_loop` avec `tokio::time::interval`
    - [X] `count_available_servers` : `SCAN` + `HGET status`
    - [X] `spawn_server`

--- orchestrator/src/main.rs
+++ orchestrator/src/main.rs
@@ -1,5 +1,6 @@
 use std::process::Command;
 use std::sync::atomic::{AtomicUsize, Ordering};
+use std::time::Duration;
 use tokio::net::UdpSocket;
 use shared::Heartbeat;
 
@@ -60,6 +61,7 @@ async fn heartbeat_listener (client : redis::Client){
                 .arg("zone").arg(&heartbeat.zone)
                 .arg("player_count").arg(heartbeat.player_count)
                 .arg("max_players").arg(heartbeat.max_players)
+                .arg("status").arg(if heartbeat.player_count < heartbeat.max_players { "available" } else { "full" })
                 .query(&mut con).expect("Failed to update Redis");
 
             let _ : () = redis::cmd("EXPIRE")
@@ -72,13 +74,52 @@ async fn heartbeat_listener (client : redis::Client){
     }
 }
 
+fn count_available_servers (con: &mut redis::Connection) -> usize {
+    let keys : Vec<String> = redis::cmd("KEYS")
+        .arg("server:*")
+        .query(con)
+        .expect("Failed to query Redis for server keys");
+
+    let mut count = 0;
+    for key in keys {
+        let status : String = redis::cmd("HGET")
+            .arg(&key)
+            .arg("status")
+            .query(con)
+            .expect("Failed to query Redis for server status");
+
+        if status == "available" {
+            count += 1;
+        }
+    }
+    count
+}
+
+async fn scaler_loop(client: redis::Client) {
+    let mut interval = tokio::time::interval(Duration::from_secs(CHECK_TIME_SERVERS_AVAILABLE as u64));
+    let mut con = client.get_connection().expect("Failed to connect to Redis");
+
+    loop {
+        interval.tick().await;
+        let available_servers = count_available_servers(&mut con);
+        println!("Available servers: {}", available_servers);
+
+        for _ in available_servers..HOT_SERVERS_MIN {
+            start_servers().await;
+        }
+    }
+}
+
 #[tokio::main]
 async fn main() {
     let client = redis::Client::open("redis://127.0.0.1:6379").expect("Failed to create Redis client");
+    let client2 = client.clone();
 
     tokio::spawn(async move {start_servers().await;});
 
     tokio::spawn(async move {heartbeat_listener(client).await;});
+
+    tokio::spawn(async move {scaler_loop(client2).await;});
 
     tokio::signal::ctrl_c().await.expect("Failed to listen for Ctrl+C");
     println!("Shutting down orchestrator...");
