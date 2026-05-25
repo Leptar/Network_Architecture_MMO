c054f601c608dd277a941aa4912920a2ab158b09
Léo Cheikh-Boukal <leo.ultimate34@gmail.com>
2026-05-25 11:23:50 -0400

    Initial commit Spatial Server

--- SpatialServer/src/main.rs
+++ SpatialServer/src/main.rs
@@ -1,3 +1,18 @@
+use bevy::{app::ScheduleRunnerPlugin, prelude::*};
+use std::time::Duration;
+// use shared::*;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Démarrage du Service Spatial...");
+
+    App::new()
+        .add_plugins(MinimalPlugins.set(ScheduleRunnerPlugin::run_loop(
+            Duration::from_secs_f64(1.0 / 60.0),
+        )))
+        .add_systems(Startup, init_spatial_service)
+        .run();
+}
+
+fn init_spatial_service() {
+    println!("Initialisation des structures spatiales...");
 }
