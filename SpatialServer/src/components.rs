1e1e8a53b2f98d50b5f263e39129da324d26e33d
Léo Cheikh-Boukal <leo.ultimate34@gmail.com>
2026-05-28 10:14:08 -0400

    Update components.rs
    Add Check_shard_transition methods (there is stub for now with the pubsub logic with brocker)

--- SpatialServer/src/components.rs
+++ SpatialServer/src/components.rs
@@ -1,4 +1,5 @@
 ﻿use bevy::prelude::*;
+use crate::quadtree::QuadTree;
 
 #[derive(Component)]
 pub struct ClientId(pub u32);
@@ -8,3 +9,31 @@ pub struct Position(pub Vec2);
 
 #[derive(Component)]
 pub struct CurrentShard(pub Option<u32>);
+
+fn check_shard_transitions(
+    mut query: Query<(&Position, &mut CurrentShard), Changed<Position>>,
+    quad_tree: Res<QuadTree>)
+{
+
+    for (position, mut currentShard) in query.iter_mut() {
+        let actual_shard = quad_tree.shard_for(position.0);
+
+        if actual_shard != currentShard.0{
+            if let Some(ancien_id) = currentShard.0 {
+                println!("Unsubscribe du shard: {}", ancien_id.to_string());
+            }
+
+            if let Some(new_Id) = actual_shard {
+                println!("Subscribe du shard: {}", new_Id.to_string());
+            }
+
+            currentShard.0 = actual_shard;
+        }
+
+        let shard_near = quad_tree.shards_near(position.0, 5.0 ); //Nombre pas defini
+
+        if shard_near.len() > 1 {
+            println!("Alerte de frontière franchie pour le joueur ! Shards impliqués : {:?}", shard_near);
+        }
+    }
+}
