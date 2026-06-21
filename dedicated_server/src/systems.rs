cc889ad21c28869c2be700fcad77d8efaf4ed3db
Léo Cheikh-Boukal <leo.ultimate34@gmail.com>
2026-06-20 22:23:54 -0400

    Update systems.rs
    add logic for dgs and read spawn player

--- dedicated_server/src/systems.rs
+++ dedicated_server/src/systems.rs
@@ -67,17 +67,20 @@ pub fn receive_packets(
                             0x55 => { //format min_x min_y max_x max_y en float32
                                 println!("Receive my working zone");
 
-                                if msg_data.len() != 16 {
+                                if msg_data.len() != 20 {
                                     println!("Invalid working zone data length : {}, expected 16", msg_data.len());
                                     return;
                                 }
 
-                                let min_x = f32::from_le_bytes([msg_data[0], msg_data[1], msg_data[2], msg_data[3]]);
-                                let min_y = f32::from_le_bytes([msg_data[4], msg_data[5], msg_data[6], msg_data[7]]);
-                                let max_x = f32::from_le_bytes([msg_data[8], msg_data[9], msg_data[10], msg_data[11]]);
-                                let max_y = f32::from_le_bytes([msg_data[12], msg_data[13], msg_data[14], msg_data[15]]);
+                                let shard_id = u32::from_le_bytes([msg_data[0], msg_data[1], msg_data[2], msg_data[3]]);
+                                let min_x = f32::from_le_bytes([msg_data[4], msg_data[5], msg_data[6], msg_data[7]]);
+                                let min_y = f32::from_le_bytes([msg_data[8], msg_data[9], msg_data[10], msg_data[11]]);
+                                let max_x = f32::from_le_bytes([msg_data[12], msg_data[13], msg_data[14], msg_data[15]]);
+                                let max_y = f32::from_le_bytes([msg_data[16], msg_data[17], msg_data[18], msg_data[19]]);
+
                                 println!("Working zone received : min_x : {}, min_y : {}, max_x : {}, max_y : {}", min_x, min_y, max_x, max_y);
 
+                                server_config.zone = shard_id.to_string();
                                 server_config.min_x = min_x;
                                 server_config.min_y = min_y;
                                 server_config.max_x = max_x;
@@ -113,31 +116,43 @@ pub fn receive_packets(
                                     println!("Received invalid client input message");
                                 }
                             },
-                            0x51 => { //New Player format : client_id (u32), init_x, init_y (float 32)
-                                println!("Receive new player");
+                            TAG_ADMIN_ROUTE_RECEIVE => {
+                                if msg_data.is_empty() { return; }
 
-                                if msg_data.len() != 12 {
-                                    println!("Invalid new player data length : {}, expected 12", msg_data.len());
-                                    return;
+                                let internal_tag = msg_data[0];
+                                let payload = &msg_data[1..];
+
+                                match internal_tag {
+                                    0x51 => {
+                                        println!("Receive new player via admin");
+
+                                        if payload.len() != 12 {
+                                            println!("Invalid new player data length : {}, expected 12", payload.len());
+                                            return;
+                                        }
+
+                                        let client_id = u32::from_le_bytes([payload[0], payload[1], payload[2], payload[3]]);
+                                        let init_x = f32::from_le_bytes([payload[4], payload[5], payload[6], payload[7]]);
+                                        let init_y = f32::from_le_bytes([payload[8], payload[9], payload[10], payload[11]]);
+                                        println!("New player received : client_id : {}, init_x : {}, init_y : {}", client_id, init_x, init_y);
+
+                                        let new_player_entity = PlayerEntity {
+                                            id: client_id,
+                                            authority: EntityAuthority::PendingHandoff,
+                                            position: Vec2::new(init_x, init_y),
+                                            rotation: 0.0,
+                                            velocity: Vec2::ZERO,
+                                        };
+
+                                        player_registry.register_player(new_player_entity);
+                                        println!("Player registered in registry, current player count : {}", player_registry.players.len());
+                                    },
+
+                                    // TODO : PAYLOAD_CROSSING_ALERT 
+                                    _ => {
+                                        println!("Unknown internal tag in Admin: {}", internal_tag);
+                                    }
                                 }
-
-                                let client_id = u32::from_le_bytes([msg_data[0], msg_data[1], msg_data[2], msg_data[3]]);
-                                let init_x = f32::from_le_bytes([msg_data[4], msg_data[5], msg_data[6], msg_data[7]]);
-                                let init_y = f32::from_le_bytes([msg_data[8], msg_data[9], msg_data[10], msg_data[11]]);
-                                println!("New player received : client_id : {}, init_x : {}, init_y : {}", client_id, init_x, init_y);
-
-                                let new_player_entity = &mut PlayerEntity {
-                                    id: client_id,
-                                    authority: EntityAuthority::PendingHandoff,
-                                    position: Vec2::new(init_x, init_y),
-                                    rotation: 0.0,
-                                    velocity: Vec2::ZERO,
-                                };
-
-                                player_registry.register_player(new_player_entity.clone());
-                                println!("Player registered in registry, current player count : {}", player_registry.players.len());
-
-                                return;
                             },
                             _ => {
                                 println!("Received message with unknown tag : {}, from connection id : {}, with stream id : {}, data : {:?}", msg_tag, connection.connection_id, stream.stream_id, msg_data);
@@ -177,10 +192,23 @@ pub fn receive_packets(
                 }
                 
                 //Stream du Broker
-                if let Some(broker_conn) = &socket.connection_broker {
+                if let Some(broker_conn) = &socket.connection_broker.clone() {
                     if (connection.connection_id == broker_conn.connection_id) {
                         socket.stream_broker = Some(stream.clone());
                         println!("Stream created with Broker, connection id : {}, stream id : {}", connection.connection_id, stream.stream_id);
+
+                        let mut auth_packet = Vec::new();
+                        auth_packet.push(TAG_ADMIN_CONNECT); // Tag 0x0A
+
+                        // Le DGS s'identifie avec son UUID unique (ex: "dgs_550e8400...")
+                        let name_str = format!("dgs_{}", server_config.id);
+                        let mut name_bytes = [0u8; 32];
+                        let bytes = name_str.as_bytes();
+                        name_bytes[..bytes.len().min(32)].copy_from_slice(&bytes[..bytes.len().min(32)]);
+                        auth_packet.extend_from_slice(&name_bytes);
+
+                        let _ = socket.peer.send(broker_conn, &stream, bytes::Bytes::from(auth_packet));
+                        println!("Identification VIP envoyée au Broker : {}", name_str);
 
                         //Allumer et connecter à tout le monde passage en idle
                         server_config.state = ServerState::Idle;
